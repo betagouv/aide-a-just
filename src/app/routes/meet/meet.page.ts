5309767b23be70c329f1b586c3381340c4960507
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2025-10-28 12:11:41 +0100

    calendly

--- src/app/routes/meet/meet.page.ts
+++ src/app/routes/meet/meet.page.ts
@@ -23,4 +23,33 @@ export class MeetPage {
   constructor(private title: Title) {
     this.title.setTitle('Rencontrer un expert | A-Just');
   }
+
+  ngAfterViewInit() {
+    const my_awesome_script = document.createElement('script');
+    my_awesome_script.setAttribute('type', 'text/javascript');
+    my_awesome_script.setAttribute(
+      'src',
+      'https://assets.calendly.com/assets/external/widget.js'
+    );
+    document.head.appendChild(my_awesome_script);
+
+    this.loadCalendly();
+  }
+
+  loadCalendly() {
+    // @ts-ignore
+    if (window.Calendly) {
+      // @ts-ignore
+      window.Calendly.initInlineWidget({
+        url: 'https://calendly.com/support-a-just/support?hide_gdpr_banner=1',
+        parentElement: document.getElementById('calendly'),
+        prefill: {},
+        utm: {},
+      });
+    } else {
+      setTimeout(() => {
+        this.loadCalendly();
+      }, 100);
+    }
+  }
 }
